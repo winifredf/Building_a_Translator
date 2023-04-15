bab147a1c6e320ff6726f1a5567931eef35a4cd3
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-15 16:11:01 -0400

    created for loop and if statements.

--- translate.py
+++ translate.py
@@ -1,2 +1,8 @@
 def translate(phrase):
     translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            traslation = translation + "d"
+        else:
+            translation = translation + letter
+            
