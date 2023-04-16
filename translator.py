6bf0a9c3f3ec56b3a92635f91d714daae6114948
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-16 11:10:00 -0400

    added .lower to letter line 4

--- translator.py
+++ translator.py
@@ -1,7 +1,7 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
+        if letter.lower() in "AEIOUaeiou":
             traslation = translation + "d"
         else:
             translation = translation + letter
