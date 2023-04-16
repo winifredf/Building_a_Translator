f1c92abc0b18f704f1930d3829002c78a244c57f
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-16 11:12:41 -0400

    corrected mispelled translation added n line 5

--- translator.py
+++ translator.py
@@ -2,7 +2,7 @@ def translate(phrase):
     translation = ""
     for letter in phrase:
         if letter.lower() in "AEIOUaeiou":
-            traslation = translation + "d"
+            translation = translation + "d"
         else:
             translation = translation + letter
     return translation
